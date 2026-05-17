81c3d6ad85375ff6aa7ccb20f61796793e9bb1f3
Ktm Town <pragyanjava@gmail.com>
2026-05-17 05:36:50 -0400

    first commit

---
+++ src/main/java/stream/Stream1.java
@@ -0,0 +1,10 @@
+package stream;
+
+import java.util.List;
+
+public class Stream1 {
+    public static void main(String[] args) {
+        List<String> states = List.of("virginia", "texas","maryland","maine","tennessee", "vermont","arizona");
+
+    }
+}
