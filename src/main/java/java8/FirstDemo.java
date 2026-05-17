3274ce28d70e49b9012ca8eb027f9c4678e36f18
Ktm Town <pragyanjava@gmail.com>
2026-05-17 05:22:04 -0400

    first commit

---
+++ src/main/java/java8/FirstDemo.java
@@ -0,0 +1,26 @@
+package java8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class FirstDemo {
+    public static void main(String[] args) {
+        List<Integer> li =Arrays.asList(66,32,45,12,20);
+            for(int i =0; i <li.size();i++){
+                System.out.println(li.get(i));
+            }
+
+        System.out.println("===================");
+            for(Integer i:li)
+                if(i>20)
+                System.out.println(i);
+
+        System.out.println("=========================");
+        System.out.println("=----=000000");
+        li.stream()
+                .filter(i->i>32)
+                .forEach(i -> System.out.println(i));
+    }
+}
